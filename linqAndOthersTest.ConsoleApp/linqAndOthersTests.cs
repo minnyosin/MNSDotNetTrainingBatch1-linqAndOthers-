6273ab7168b5d2e6c5a0d1d62a43806a590eef62
Min Nyo Sin <minnyosin.dev@gmail.com>
2025-04-21 00:22:30 +0630

    Add project files.

---
+++ linqAndOthersTest.ConsoleApp/linqAndOthersTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNSDotNetTrainingBatch1.ConsoleApp
+{
+    internal class linqAndOthersTests
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            var evenNumbers = numbers.Where(nums => nums % 2 == 0);
+
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
